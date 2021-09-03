c1fe83c02f2e13bd49e900c9bc3d06595a59fe81
gyongyiovics <gyongyimako@gmail.com>
2021-09-03 19:33:28 +0200

    added versionNumber field, constructor, getter and setter

--- src/main/model/humanoids/Android.java
+++ src/main/model/humanoids/Android.java
@@ -1,7 +1,24 @@
 package main.model.humanoids;
 
+import java.time.LocalDate;
+
 public class Android extends Humanoid {
+    private Double versionNumber;
 
-    // TODO
+    public Android(Double versionNumber) {
+        this.versionNumber = versionNumber;
+    }
 
+    public Android(String lastName, String firstName, LocalDate birth, Double versionNumber) {
+        super(lastName, firstName, birth);
+        this.versionNumber = versionNumber;
+    }
+
+    public Double getVersionNumber() {
+        return versionNumber;
+    }
+
+    public void setVersionNumber(Double versionNumber) {
+        this.versionNumber = versionNumber;
+    }
 }
