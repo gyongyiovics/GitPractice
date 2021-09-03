f8bdd15415bb7bb1b675fe13c2bb8558815afb45
banjudy <banjudy@gmail.com>
2021-09-03 19:39:10 +0200

    Added new class-Politicians

---
+++ src/main/model/humanoids/Politicians.java
@@ -0,0 +1,11 @@
+package main.model.humanoids;
+
+import main.model.animals.Animal;
+
+public class Politicians extends Humanoid{
+
+    @Override
+    public void feedPets() {
+        System.out.println("Here is your penny, buddy, the rest is mine!");
+    }
+}
