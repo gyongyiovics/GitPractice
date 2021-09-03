1c955a2c30845e1fdc82a1c883bacf3e971da6db
gyongyiovics <gyongyimako@gmail.com>
2021-09-03 19:27:51 +0200

    added gender field with getter and setter

--- src/main/model/humanoids/Humanoid.java
+++ src/main/model/humanoids/Humanoid.java
@@ -12,6 +12,7 @@ public class Humanoid implements HumanoidInterface {
     protected String firstName;
     protected LocalDate birth;
     protected boolean isSad;
+    protected Gender gender;
 
     protected List<Animal> petsOwned;
 
@@ -43,6 +44,10 @@ public class Humanoid implements HumanoidInterface {
         return petsOwned;
     }
 
+    public Gender getGender() {
+        return gender;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -61,6 +66,10 @@ public class Humanoid implements HumanoidInterface {
 
     public void setPetsOwned(List<Animal> petsOwned) {
         this.petsOwned = petsOwned;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
     }
 
     @Override
